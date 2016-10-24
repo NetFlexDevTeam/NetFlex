476bb27aee3f8933080ccd3a13257f734b925296
RD02220 <rd02220@georgiasouthern.edu>
2016-10-24 18:32:07 -0400

    Add files via upload
    This is the original setup for the files.

---
+++ Netflex.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Netflex - and chill</title>
+		<link href="NetflexStyle.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="http://deberardino.com/favicon.ico" />
+ <script src="NetflexJS.js"></script>
+ <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+	</head>
+<body>
+	
+Netflex
+
+<todolist><br/>
+Registration Process (Email confirmation/Text based confirmation)<br/>
+Login Process (PHP Session Store - @Ryan suggestion we can do something else)<br/>
+Forgot Password Process (Email/Text code confirmation modal page)<br/>
+Logout Process (easy with php, simply .php function you call then loop back to main home page<br/>
+Code Video Sliders (Javascript/AJAX enabled)<br/>
+Code Video Players (Will research a few AJAX video based APIS and we can like fill every video with rick roll or something<br/>
+Code Administrative Pages:<br/>
+&nbsp&nbsp&nbsp&nbspAdd Title/ Edit Title/ Delete Title<br/>
+&nbsp&nbsp&nbsp&nbspEdit Users/ Delete Users<br/>
+Code Client Pages:<br/>
+&nbsp&nbsp&nbsp&nbspEdit Account Settings/ Delete Account<br/>
+Design the clients header while both logged in and logged out<br/>
+Design the footer while both logged in and out<br/>
+Design the Footer info filler pagers ( About us/ Contact Us/ etc)<br/>
+<font style="color:black">HTACCESS file to remove file extensions</font><br/>
+<font style="color:black">Design site Favicon (Icon in user tabs)</font><br/>
+More to come.
+</todolist>
+
+<footer>
+Line 1<br/>
+Line 2<br/>
+Line 3<br/>
+</footer>
+</body>
+</html>
